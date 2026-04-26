30cf19f27c5bcdae1e90abcb2c0e1c877e5a567d
Letionc <libaichuanphigros@126.com>
2026-04-26 15:21:55 +0800

    update config; optimize mdLookScript.js

--- configList.js
+++ configList.js
@@ -1,3 +1,4 @@
 const CONFIG_LIST = Object.entries({
     "./chinese.md": "语文",
+    "./russian.md": "俄语(非正版)",
 });
--- mdLookScript.js
+++ mdLookScript.js
@@ -1,13 +1,18 @@
 const ELEM_LOOK_BOX = document.getElementById("look-box");
+const show_history = {};
 
 function markdownShow(fn) {
+    const history = show_history;
+    if(history){
+        ELEM_LOOK_BOX.innerHTML = history;
+    }
     fetch(fn)
     .then(e=>e.text())
     .then(md=>{
-        ELEM_LOOK_BOX.innerHTML = compileMdToHtml(md);
+        ELEM_LOOK_BOX.innerHTML = show_history[fn] = compileMdToHtml(md);
     })
     .catch(e=>{
         console.error(e);
-        alert("mdLookScript.js：不好！获取网络内容时发生错误了！\n"+String(e));
+        alert("mdLookScript.js: 不好! 获取网络内容时发生错误了! \n"+String(e));
     });
 }
