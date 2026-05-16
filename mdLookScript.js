9b542c4dc12c373d3209a1a49076cb30bab79509
Letionc <libaichuanphigros@126.com>
2026-05-16 18:26:31 +0800

    add feature: turnLight

--- mdLookScript.js
+++ mdLookScript.js
@@ -1,5 +1,7 @@
 const ELEM_LOOK_BOX = document.getElementById("look-box");
+const ELEM_LIGHT_BTN = document.getElementById("light-btn");
 const show_history = {};
+var is_light_on = true;
 
 function markdownShow(fn) {
     const history = show_history;
@@ -9,10 +11,20 @@ function markdownShow(fn) {
     fetch(fn)
     .then(e=>e.text())
     .then(md=>{
-        ELEM_LOOK_BOX.innerHTML = show_history[fn] = compileMdToHtml(md);
+        ELEM_LOOK_BOX.innerHTML = show_history[fn] = compileMdToHtml(md) + '<div class="page-margin"></div>';
     })
     .catch(e=>{
         console.error(e);
         alert("mdLookScript.js: 不好! 获取网络内容时发生错误了! \n"+String(e));
     });
+}
+function turnLight() {
+    ELEM_LIGHT_BTN.innerHTML = (is_light_on? "Turn On": "Turn Off");
+    if(is_light_on){
+        is_light_on = false;
+        ELEM_LOOK_BOX.classList.add("unlighted");
+    }else{
+        is_light_on = true;
+        ELEM_LOOK_BOX.classList.remove("unlighted");
+    }
 }
